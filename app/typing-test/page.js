3469f342ca6cba6849d113b20101cddc131586d8
ISHIMWE Konnie <konniemavibes@gmail.com>
2026-05-31 15:41:38 +0200

    curly bracket i forgot

--- app/typing-test/page.js
+++ app/typing-test/page.js
@@ -147,6 +147,7 @@ export default function TypingTestPage() {
           </button>
         </div>
       </nav>
+      </RainbowNavWrapper>
 
       <section className="pt-24 pb-16 px-4 sm:px-6 lg:px-8">
         <div className="max-w-6xl mx-auto">
